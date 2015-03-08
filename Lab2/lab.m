c6fda8b48827e28cb3c4a0e9e6660e7cb6b511db
Guilherme Souza <sitegui@sitegui.com.br>
2015-03-08 01:36:09 -0300

    Formatação e tópico pedidos

--- Lab2/lab.m
+++ Lab2/lab.m
@@ -1,41 +1,44 @@
-close all;
-clear all;
-clc;
-format short;
+%% Lab 2 - Pré Relatório
+% Intro
 
-s=tf('s');
+%% Valores dos resistores
+z1 = 100e3;
+z2 = 10e3;
+z3 = 100e3;
+z4 = 220e3;
+z5 = 100e3;
+z6 = 470e3;
+z7 = 1e6;
+c1 = 0.1e-6;
+c2 = 0.1e-6;
+c3 = 0.1e-6;
 
-%os valores dos resistores e capacitores
-z1=100e3;
-z2=10e3;
-z3=100e3;
-z4=220e3;
-z5=100e3;
-z6=470e3;
-z7=1e6;
-c1=0.1e-6;
-c2=0.1e-6;
-c3=0.1e-6;
+%% Funções de transferência de cada estágio
+% As equaçes foram obtidas pelo circuito já transformado para a frequência por Laplace
+s = tf('s');
+G1 = tf(-z2/z1)
+G2 = -z4/(z3*(z4*c1*s+1))
+G3 = -z6/(z5*(z6*c2*s+1))
+G4 = -1/(z7*c3*s)
 
-%v1 e a saida da 1a etapa, v2 da 2a, v3 da 3a e y da ultima.
-%as equacoes foram obtidas pelo circuito ja transformado para a frequencia
-%por laplace
-v1=-z2/z1*(s/s); %s/s foi migue pra funcionar a resposta ao quadrado
-v1=minreal(v1)
-v2=-v1*z4/(z3*z4*c1*s+z3)
-v3=-v2*z6/(z5*z6*c2*s+z5)
-y=-v3/(z7*c3*s)
+%% Função de trasferência completa
+G = G1*G2*G3*G4
 
-%margem de fase e ganho
-margin (y)
+%% Diagrama de bode
+% Incluindo margens de fase e ganho do sistema
+margin(G);
 
-%para fazer a resposta a entrada quadrada
-t = 0:.0001:5;
+%% Entrada
+% Uma onda quadrada de 1 Hz e amplitude 1 (volt)
+t = 0:.0001:4;
 r = square(2*pi*t)*0.5+0.5;
-%figure, plot(t,r);
+plot(t,r);
+axis([0 4 -0.1 1.1]);
+xlabel('t (s)');
+ylabel('r (V)');
 
 %malha aberta
-figure, lsim(v1,r,t)
-figure, lsim(v2,r,t)
-figure, lsim(v3,r,t)
-figure, lsim(y,r,t)
+% figure, lsim(v1,r,t)
+% figure, lsim(v2,r,t)
+% figure, lsim(v3,r,t)
+% figure, lsim(y,r,t)
