3161674cc20536a69988802225b9bc26bf583815
Guilherme Souza <sitegui@sitegui.com.br>
2015-03-29 01:47:48 -0300

    Formatação do relatorio3.m

--- lab3/relatorio3.m
+++ lab3/relatorio3.m
@@ -13,7 +13,6 @@ load ../pids.mat
 load sinais_tratados.mat
 s = tf('s');
 
-
 %% controlador proporcional
 A = G.num{1}(4);
 den = G.den{1};
@@ -26,50 +25,40 @@ dt = 0.001;
 t = 0:dt:2;
 rampa = t;
 sistema = feedback(k*G, 1);
-%lsim(sistema, rampa, t);
 
 %%
 % Entrada quadrada de amplitude 1 e 0.25Hz
 onda_quadrada = square(2*pi*0.25*t)*0.5+0.5;
-%plot(t, onda_quadrada);
-%ylim([-0.1, 1.1]);
-%title('Entrada quadrada');
-%xlabel('t (s)');
 
 %%
 % Resposta à onda quadrada
-tempo1=linspace(0, 2.5, length(saida1));
+tempo1 = linspace(0, 2.5, length(saida1));
 
 lsim(sistema, k*(1-sistema), 'b -.', onda_quadrada, t);
 xlim([0, 1]);
 hold on
-plot (tempo1,saida1, 'r');
-plot (tempo1,controle1, 'r -.');
+plot(tempo1,saida1, 'r');
+plot(tempo1,controle1, 'r -.');
 title('Reposta à onda quadrada');
 legend('y(t)', 'u(t)', 'y(t) real', 'u(t) real');
 hold off;
 
-
 %%
 % Entrada de rampa
 onda_rampa = cumsum(onda_quadrada)*dt;
-%figure, plot(t, onda_rampa);
-%title('Entrada em rampa');
-%xlabel('t (s)');
 
 %%
 % Resposta à rampa
 figure, lsim(sistema, 'b', onda_rampa, t);
 xlim([0, 1]);
 hold on
-lsim (k*(1-sistema), 'b -.', onda_rampa, t);
-lsim(tf(1,[1 0]),'r',saida1,tempo1);
-lsim(tf(1,[1 0]),'r -.',controle1,tempo1);
+lsim(k*(1-sistema), 'b -.', onda_rampa, t);
+lsim(tf(1,[1 0]), 'r', saida1, tempo1);
+lsim(tf(1,[1 0]), 'r -.', controle1, tempo1);
 title('Reposta à rampa');
 legend('y(t)', 'u(t)', 'y(t) real', 'u(t) real');
 hold off
-figure,
-
+figure;
 
 %% Controlador de Ziegler Nichols
 % O primeiro passo no método de Ziegler Nichols é encontrar $k = k_{osc}$
@@ -87,8 +76,7 @@ figure,
 %
 % Com o critério de Routh chega-se em: $k = \frac{0.03622}{0.0003019 *
 % 8.993} = 13.3408$
-% 
-% 
+%
 k_osc = 13.3408;
 GC_mf = feedback(G*k_osc,1);
 poles = esort(pole(GC_mf));
@@ -104,25 +92,20 @@ C = kp*(1 + 1/(Ti*s) + Td*s)
 % controlador, obtemos $kp_2 = 1.31 kp$.
 %
 % Comparação antes vs depois da alteração manual do lugar dos pólos:
-% 
-% % % % <<../imgs/ziegler-nichols.png>>
+%
 kp_2 = 1.31*kp;
 C_2 = 1.31*C/(0.0001*s+1)
 
 %%
 % Cálculo dos valores de desempenho
 sistema = feedback(G*C_2, 1);
-%%
-% % % % % <<../imgs/ziegler-nichols_step.png>>
 
-%% 
+%%
 % Desempenho do sistema discreto
 Ts = 0.001;
 Gz = c2d(G, Ts, 'zoh');
 Cz = c2d(C_2, Ts, 'matched')
 sistemaZ = feedback(Gz*Cz, 1);
-%%
-% % % % % <<../imgs/ziegler-nichols_z_step.png>>
 
 %%
 % Simulação do sistema discreto
@@ -131,12 +114,13 @@ ylim([-2, 2]);
 xlim([0, 1]);
 hold on
 lsim(Cz*(1-sistemaZ), onda_quadrada, t, 'b -.');
-plot (tempo1, saida2, 'r');
-plot (tempo1, controle2, 'r -.');
+plot(tempo1, saida2, 'r');
+plot(tempo1, controle2, 'r -.');
 title('Reposta à onda quadrada');
 legend('y(t)', 'u(t)', 'y(t) real', 'u(t) real');
 hold off;
 figure;
+
 %%
 lsim(sistemaZ,onda_rampa,t,'b');
 ylim([-2, 2]);
@@ -158,27 +142,28 @@ Czn;
 
 Cz2 = c2d(Csiso/(s*0.0001+1), Ts, 'matched')
 
-sistemaZ2=feedback(Gz*Cz2,1);
+sistemaZ2 = feedback(Gz*Cz2,1);
 
 figure, lsim(sistemaZ2, onda_quadrada, t, 'b');
 ylim([-2, 2]);
 xlim([0, 1]);
 hold on
 lsim(Cz2*(1-sistemaZ2), onda_quadrada, t, 'b -.');
-plot (tempo1, saida3, 'r');
-plot (tempo1, controle3, 'r -.');
+plot(tempo1, saida3, 'r');
+plot(tempo1, controle3, 'r -.');
 title('Reposta à onda quadrada');
 legend('y(t)', 'u(t)', 'y(t) real', 'u(t) real');
 hold off;
 figure;
+
 %%
-figure, lsim(sistemaZ2,onda_rampa,t,'b');
+figure, lsim(sistemaZ2, onda_rampa, t, 'b');
 ylim([-2, 2]);
 xlim([0, 1]);
 hold on
-lsim(Cz2*(1-sistemaZ2),onda_rampa,t,'b -.');
-lsim(tf(1,[1 0]),'r',saida3,tempo1);
-lsim(tf(1,[1 0]),'-. r',controle3,tempo1);
+lsim(Cz2*(1-sistemaZ2), onda_rampa, t, 'b -.');
+lsim(tf(1,[1 0]), 'r', saida3, tempo1);
+lsim(tf(1,[1 0]), '-. r', controle3, tempo1);
 title('Reposta à rampa');
 legend('y(t)', 'u(t)', 'y(t) real', 'u(t) real');
 hold off;
