532d71c8d4366435f2f49cef8c523620fe102810
Guilherme Souza <sitegui@sitegui.com.br>
2015-04-26 17:45:48 -0300

    Mais no matlab do pré relatório 6

--- lab6/pre_relatorio.m
+++ lab6/pre_relatorio.m
@@ -7,8 +7,8 @@
 % * Guilherme de Oliveira Souza – 117093
 % * Vinícius Ragazi David - 120258
 
-load ../G.mat
 clear all
+load ../G.mat
 
 %% Localização dos polos
 % Deseja-se que o tempo de estabilização seja de 0.5s e o coeficiente de
@@ -30,4 +30,22 @@ p2 = omega_n*(-xi-i*sqrt(1-xi*xi))
 %%
 % O terceiro polo foi escolhido arbitrariamente como sendo em -30
 p3 = -30;
-p = [p1, p2, p3];
+p = [p1; p2; p3];
+
+%% Modelo de estados
+Gss = ss(G)
+
+%% Determinação da matriz K
+K = place(Gss.a, Gss.b, p)
+
+%% Pólos do observador
+% Os pólos do observador devem ser escolhidos de forma que o estado
+% estimado siga o estado verdadeiro o mais rápido possível. Para isso, eles
+% devem estar mais à esqueda que os pólos do controlador.
+%
+% Dessa forma, foi escolhido arbitrariamente que a parte real deveria ser
+% 50% mais negativa
+po = p+real(p)*0.5
+
+%% Determinação da matriz L
+L = place(Gss.a', Gss.c', po)'
