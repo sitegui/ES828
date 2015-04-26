1c105faa6a0883118af97b8a4c2c0e53e03b457d
Guilherme Souza <sitegui@sitegui.com.br>
2015-04-26 17:19:03 -0300

    Início do pré relatório 6

---
+++ lab6/pre_relatorio.m
@@ -0,0 +1,33 @@
+%% ES868 – Pré Relatório Lab 6
+% *Controle por realimentação de saída de uma planta eletrônica*
+%
+% Turma A - 26/04/2015
+%
+% * Augusto Miranda Garcia - 104627
+% * Guilherme de Oliveira Souza – 117093
+% * Vinícius Ragazi David - 120258
+
+load ../G.mat
+clear all
+
+%% Localização dos polos
+% Deseja-se que o tempo de estabilização seja de 0.5s e o coeficiente de
+% amortecimento seja $\sqrt{2}/2$
+%
+% Dessa forma, $\xi = \sqrt{2}/2$
+t_e = 0.5;
+xi = sqrt(2)/2
+
+%%
+% Como $t_e = 4 / (\xi \cdot \omega_n)$, obtém-se
+omega_n = 4/t_e/xi
+
+%%
+% Dessa forma, os dois pólos dominantes serão
+p1 = omega_n*(-xi+i*sqrt(1-xi*xi))
+p2 = omega_n*(-xi-i*sqrt(1-xi*xi))
+
+%%
+% O terceiro polo foi escolhido arbitrariamente como sendo em -30
+p3 = -30;
+p = [p1, p2, p3];
